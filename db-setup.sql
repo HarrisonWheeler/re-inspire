4ba0ae8710adc3cce486ea67abf8cef774ac2b1f
Harrison Wheeler <wheelerharrison93@gmail.com>
2020-09-28 10:28:36 -0600

    db linked and SQL table and created

---
+++ db-setup.sql
@@ -0,0 +1,12 @@
+-- CREATE TABLE quotes (
+--   id int NOT NULL AUTO_INCREMENT,
+--   name VARCHAR (255) NOT NULL,
+--   description VARCHAR (255) NOT NULL,
+--   isExplicit TINYINT,
+--   PRIMARY KEY (id)
+-- );
+
+-- INSERT INTO quotes (name, description, isExplicit)
+-- VALUES ("Amanda Wheeler", "Go ahead. Do it. Drink that milk!", 0)
+
+SELECT * FROM quotes
